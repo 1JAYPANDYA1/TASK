2cb4fc24b6db607b781b550e18b9e0ceb6de784e
1JAYPANDYA1 <jaypandya2299@gmail.com>
2024-12-16 12:22:08 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {browserRouter as Router,Routes,Route,Link} from 'react-router-dom'
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route to="/"></Route>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
